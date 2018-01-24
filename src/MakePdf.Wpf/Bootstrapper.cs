02a2239e0e330a7337c7c5dd7f91a26ba2d7e131
kuttsun <tsukushimail+github@gmail.com>
2018-01-24 16:13:11 +0900

    Use Prism Region

---
+++ src/MakePdf.Wpf/Bootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Microsoft.Practices.Unity;
+
+using Prism.Mvvm;
+using Prism.Unity;
+
+using MakePdf.Wpf.Views;
+
+namespace MakePdf.Wpf
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            // ここでViewModelを差し込む
+            // ここでやらないと、RegionManager に Region が生成される前に ViewModel が生成されたりして厄介なため
+            //ViewModelLocator.SetAutoWireViewModel(Shell, true); 
+
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            // View を全てコンテナに登録しておく (RegionManager で使うため)
+            Container.RegisterTypeForNavigation<Menu>("Menu");
+            Container.RegisterTypeForNavigation<HomePage>("HomePage");
+
+            //    // Viewを全てobject型としてコンテナに登録しておく（RegionManagerで使うため）
+            //    this.Container.RegisterTypes(
+            //        AllClasses.FromLoadedAssemblies().Where(t => t.Namespace.EndsWith(".Views")),
+            //        getFromTypes: _ => new[] { typeof(object) },
+            //        getName: WithName.TypeName);
+        }
+    }
+}
--- src/MakePdf.Wpf/Views/Shell.xaml.cs
+++ src/MakePdf.Wpf/Views/Shell.xaml.cs
@@ -10,19 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-using MakePdf.Wpf.ViewModels;
 
 namespace MakePdf.Wpf.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Shell.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Shell : Window
     {
-        public MainWindow()
+        public Shell()
         {
             InitializeComponent();
         }
