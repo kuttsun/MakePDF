61607c288228212d8b5cd5d6d116789da26aa8cc
kuttsun <tsukushimail+github@gmail.com>
2018-01-25 13:08:33 +0900

    Add EasyMode.Input UI

--- src/MakePdf.Wpf/ViewModels/EasyMode/InputViewModel.cs
+++ src/MakePdf.Wpf/ViewModels/EasyMode/InputViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 using Prism.Commands;
 using Prism.Mvvm;
@@ -12,5 +13,24 @@ namespace MakePdf.Wpf.ViewModels.EasyMode
 {
     class InputViewModel : BindableBase
     {
+        public ObservableCollection<TargetFile> TargetFiles { get; set; } = new ObservableCollection<TargetFile>();
+
+        public InputViewModel()
+        {
+            // test
+            TargetFiles.Add(new TargetFile { Filename = "foo", Path = "foo" });
+            TargetFiles.Add(new TargetFile { Filename = "bar", Path = "bar" });
+        }
+    }
+
+    class TargetFile
+    {
+        public string Filename { get; set; }
+        public string Path { get; set; }
+
+        public TargetFile()
+        {
+
+        }
     }
 }
