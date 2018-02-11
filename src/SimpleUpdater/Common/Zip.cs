4cf9f364557b4028912f764487fd1ac998612ab6
kuttsun <tsukushimail+github@gmail.com>
2018-02-12 02:32:06 +0900

    Create Zip.cs

---
+++ src/SimpleUpdater/Common/Zip.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+using Microsoft.Extensions.Logging;
+
+namespace SimpleUpdater.Common
+{
+    public static class Zip
+    {
+        public static bool ExtractEntries(string zipFileName, string outputDir, ILogger logger = null)
+        {
+            if (Directory.Exists(outputDir) == false)
+            {
+                Directory.CreateDirectory(outputDir);
+            }
+
+            // Open the zip file, and create a ZipArchive object.
+            using (ZipArchive archive = ZipFile.OpenRead(zipFileName))
+            {
+                // Write the selected file to the specified folder.
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    try
+                    {
+                        entry.ExtractToFile($@"{outputDir}\{entry.FullName}");
+                        logger?.LogInformation($"Success: {entry.FullName}");
+                    }
+                    catch (Exception e)
+                    {
+                        logger?.LogCritical(e, $"Failure: {entry.FullName}");
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 指定したディレクトリ内のファイルとフォルダをまとめてzipにする
+        /// </summary>
+        /// <param name="targetDir"></param>
+        /// <param name="destinationFileName"></param>
+        public static bool ArchiveDir(string targetDir, string destinationFileName)
+        {
+            File.Delete(destinationFileName);
+
+            // ファイル一覧を取得
+            IEnumerable<string> files = Directory.GetFiles(targetDir, "*", SearchOption.AllDirectories);
+
+            // 順にzipに追加
+            using (var archive = ZipFile.Open(destinationFileName, ZipArchiveMode.Update))
+            {
+                foreach (string file in files)
+                {
+                    archive.CreateEntryFromFile(file, GetRelativePath(targetDir + '\\', file), CompressionLevel.Optimal);
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 引数１のディレクトリから見た引数２のファイルへの相対パスを取得する
+        /// </summary>
+        /// <param name="uri1">基準となるディレクトリへの絶対パス(最後は\で終わっている必要あり)</param>
+        /// <param name="uri2">目的のファイルへの絶対パス</param>
+        /// <returns>引数１のディレクトリから見た引数２のファイルへの相対パス</returns>
+        /// <example>
+        /// GetRelativePath(@"C:\Windows\System\", @"C:\Windows\file.txt")
+        /// ..\file.txt
+        /// </example>
+        static string GetRelativePath(string uri1, string uri2)
+        {
+            Uri u1 = new Uri(uri1);
+            Uri u2 = new Uri(uri2);
+
+            Uri relativeUri = u1.MakeRelativeUri(u2);
+
+            string relativePath = relativeUri.ToString();
+
+            relativePath.Replace('/', '\\');
+
+            return (relativePath);
+        }
+    }
+}
--- src/SimpleUpdater/UpdateManager/GitHub.cs
+++ src/SimpleUpdater/UpdateManager/GitHub.cs
@@ -5,11 +5,12 @@ using System.Linq;
 using System.Diagnostics;
 using System.Net;
 using System.IO;
-using System.IO.Compression;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Logging;
+
+using SimpleUpdater.Common;
 
 namespace SimpleUpdater.UpdateManager
 {
@@ -60,7 +61,7 @@ namespace SimpleUpdater.UpdateManager
 
             await DownloadZipAsync(zipUrl, outputPath);
 
-            ExtractEntries(outputPath, $"{appInfo.Name}-{appInfo.Version}");
+            Zip.ExtractEntries(outputPath, $"{appInfo.Name}-{appInfo.Version}");
 
             // Delete downloaded zip file.
             File.Delete(outputPath);
@@ -126,83 +127,6 @@ namespace SimpleUpdater.UpdateManager
         string GetAssetUrl(string tag, string asset)
         {
             return $"{gitHubRepository}/releases/download/{tag}/{asset}";
-        }
-
-        bool ExtractEntries(string zipFileName, string outputDir)
-        {
-            if (Directory.Exists(outputDir) == false)
-            {
-                Directory.CreateDirectory(outputDir);
-            }
-
-            // Open the zip file, and create a ZipArchive object.
-            using (ZipArchive archive = ZipFile.OpenRead(zipFileName))
-            {
-                // Write the selected file to the specified folder.
-                foreach (ZipArchiveEntry entry in archive.Entries)
-                {
-                    try
-                    {
-                        entry.ExtractToFile($@"{outputDir}\{entry.FullName}");
-                        logger?.LogInformation($"Success: {entry.FullName}");
-                    }
-                    catch (Exception e)
-                    {
-                        logger?.LogCritical(e, $"Failure: {entry.FullName}");
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// 指定したディレクトリ内のファイルとフォルダをまとめてzipにする
-        /// </summary>
-        /// <param name="targetDir"></param>
-        /// <param name="destinationFileName"></param>
-        bool ArchiveDir(string targetDir, string destinationFileName)
-        {
-            File.Delete(destinationFileName);
-
-            // ファイル一覧を取得
-            IEnumerable<string> files = Directory.GetFiles(targetDir, "*", SearchOption.AllDirectories);
-
-            // 順にzipに追加
-            using (var archive = ZipFile.Open(destinationFileName, ZipArchiveMode.Update))
-            {
-                foreach (string file in files)
-                {
-                    archive.CreateEntryFromFile(file, GetRelativePath(targetDir + '\\', file), CompressionLevel.Optimal);
-                }
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// 引数１のディレクトリから見た引数２のファイルへの相対パスを取得する
-        /// </summary>
-        /// <param name="uri1">基準となるディレクトリへの絶対パス(最後は\で終わっている必要あり)</param>
-        /// <param name="uri2">目的のファイルへの絶対パス</param>
-        /// <returns>引数１のディレクトリから見た引数２のファイルへの相対パス</returns>
-        /// <example>
-        /// GetRelativePath(@"C:\Windows\System\", @"C:\Windows\file.txt")
-        /// ..\file.txt
-        /// </example>
-        string GetRelativePath(string uri1, string uri2)
-        {
-            Uri u1 = new Uri(uri1);
-            Uri u2 = new Uri(uri2);
-
-            Uri relativeUri = u1.MakeRelativeUri(u2);
-
-            string relativePath = relativeUri.ToString();
-
-            relativePath.Replace('/', '\\');
-
-            return (relativePath);
         }
     }
 }
