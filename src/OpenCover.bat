cc2aee7ec65b0bd929c091b6fd32c8cabd8f9e66
kuttsun <tsukushimail+github@gmail.com>
2018-03-22 16:47:08 +0900

    Fix CI

--- src/OpenCover.bat
+++ src/OpenCover.bat
@@ -1,15 +1,16 @@
+@echo off
+
 set MSTEST=%1
 
 set OPENCOVER="packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"
 
 set TARGET_TEST="MakePdf.Core.Tests.dll"
 
-set TARGET_DIR=".\MakePdf.Core.Tests\bin\Release"
+set TARGET_DIR="MakePdf.Core.Tests\bin\Release"
 
 rem OpenCover output file
 set OUTPUT="OpenCover.xml"
 
-rem set FILTERS="+[OpenCoverSample]*"
 set FILTERS="+[*]*"
 
-%OPENCOVER% -register:user -target:%MSTEST% -targetargs:%TARGET_TEST% -targetdir:%TARGET_DIR% -filter:%FILTERS% -output:%OUTPUT%
+%OPENCOVER% -register:user -target:%MSTEST% -targetargs:%TARGET_TEST% -targetdir:%~dp0%TARGET_DIR% -filter:%FILTERS% -output:%~dp0%OUTPUT%
--- src/ReportGenerator.bat
+++ src/ReportGenerator.bat
@@ -1,7 +1,9 @@
+@echo off
+
 set REPORTGEN="packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe"
 
 set OUTPUT="OpenCover.xml"
 
-set OUTPUT_DIR="html"
+set OUTPUT_DIR="ReportGenerator"
 
-%REPORTGEN% -reports:%OUTPUT% -targetdir:%OUTPUT_DIR%
+%REPORTGEN% -reports:%~dp0%OUTPUT% -targetdir:%~dp0%OUTPUT_DIR%
