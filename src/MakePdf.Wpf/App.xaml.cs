3d4150c672fa306a168de43b03e85ea285f6ed68
kuttsun <tsukushimail+github@gmail.com>
2018-05-10 14:21:04 +0900

    Output contents of update to console

--- src/MakePdf.Wpf/App.xaml.cs
+++ src/MakePdf.Wpf/App.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Reflection;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -21,13 +23,23 @@ namespace MakePdf.Wpf
     /// </summary>
     public partial class App : Application
     {
-        [System.Runtime.InteropServices.DllImport("Kernel32.dll")]
-        public static extern bool AttachConsole(int processId);
+        // https://docs.microsoft.com/en-us/windows/console/attachconsole
+        [DllImport("Kernel32.dll")]
+        static extern bool AttachConsole(int processId);
+        const int ATTACH_PARENT_PROCESS = -1;
+
+        // https://docs.microsoft.com/en-us/windows/console/allocconsole
+        [DllImport("Kernel32.dll")]
+        static extern bool AllocConsole();
+
+        // https://docs.microsoft.com/en-us/windows/console/freeconsole
+        [DllImport("Kernel32.dll")]
+        static extern bool FreeConsole();
 
         [STAThread]
         public static int Main(string[] args)
         {
-            AttachConsole(-1);
+            var attachConsole = AttachConsole(ATTACH_PARENT_PROCESS);
 
             var myInfo = MyInformation.Instance;
 
@@ -35,9 +47,30 @@ namespace MakePdf.Wpf
             var mgr = Service.Provider.GetService<Updater>();
             if (mgr.CanUpdate(args))
             {
-                Console.WriteLine("Start Update.");
+                if (attachConsole == false)
+                {
+                    // Create Console
+                    if (AllocConsole() == false)
+                    {
+                        Debug.Assert(false, "Update failed");
+                        return 1;
+                    }
+
+                    var stream = Console.OpenStandardOutput();
+                    var stdout = new StreamWriter(stream)
+                    {
+                        AutoFlush = true,
+                    };
+                    Console.SetOut(stdout);
+                }                
+
                 mgr.Update(args);
                 mgr.RestartApplication(args);
+
+                if (attachConsole == false)
+                {
+                    FreeConsole();
+                }
                 return 0;
             }
 
@@ -57,26 +90,26 @@ namespace MakePdf.Wpf
             var version = cla.Option("-v|--version", "Show version", CommandOptionType.NoValue);
             var file = cla.Option("-f|--file", "Input file (CUI mode)", CommandOptionType.SingleValue);
             cla.OnExecute(() =>
-            {
-                if (version.HasValue())
                 {
-                    Console.WriteLine($"\n{myInfo.Name} {myInfo.AssemblyInformationalVersion}");
-                    return 0;
-                }
+                    if (version.HasValue())
+                    {
+                        Console.WriteLine($"\n{myInfo.Name} {myInfo.AssemblyInformationalVersion}");
+                        return 0;
+                    }
 
-                var logger = Service.Provider.GetService<ILogger<App>>();
-                logger.LogInformation($"{myInfo.Name} {myInfo.AssemblyInformationalVersion}");
+                    var logger = Service.Provider.GetService<ILogger<App>>();
+                    logger.LogInformation($"{myInfo.Name} {myInfo.AssemblyInformationalVersion}");
 
-                if (file.HasValue())
-                {
-                    var cuimode = Service.Provider.GetService<CuiMode>();
-                    return cuimode.Start(file.Value());
-                }
+                    if (file.HasValue())
+                    {
+                        var cuimode = Service.Provider.GetService<CuiMode>();
+                        return cuimode.Start(file.Value());
+                    }
 
-                App app = new App();
-                app.InitializeComponent();
-                return app.Run();
-            });
+                    App app = new App();
+                    app.InitializeComponent();
+                    return app.Run();
+                });
 
             // Execution
             try
