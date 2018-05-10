b8d0aa7fe9db02635f10ff4de119976da60d3ebb
kuttsun <tsukushimail+github@gmail.com>
2018-05-10 18:08:56 +0900

    Fix OpenCover.bat

--- src/OpenCover.bat
+++ src/OpenCover.bat
@@ -1,16 +1,14 @@
-@echo off
-
 set MSTEST=%1
 
-set OPENCOVER="packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"
+set OPENCOVER="%~dp0packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"
 
 set TARGET_TEST="MakePdf.Core.Tests.dll"
 
-set TARGET_DIR="MakePdf.Core.Tests\bin\Release"
+set TARGET_DIR=MakePdf.Core.Tests\bin\Release
 
 rem OpenCover output file
-set OUTPUT="OpenCover.xml"
+set OUTPUT=OpenCover.xml
 
 set FILTERS="+[*]*"
 
-%OPENCOVER% -register:user -target:%MSTEST% -targetargs:%TARGET_TEST% -targetdir:%~dp0%TARGET_DIR% -filter:%FILTERS% -output:%~dp0%OUTPUT%
+%OPENCOVER% -register:user -target:%MSTEST% -targetargs:%TARGET_TEST% -targetdir:"%~dp0%TARGET_DIR%" -filter:%FILTERS% -output:"%~dp0%OUTPUT%"
