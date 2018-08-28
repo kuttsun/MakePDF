b2a71881cd787b94fad557dc03f199872cb0e0fa
kuttsun <tsukushimail+github@gmail.com>
2018-08-28 13:19:22 +0900

    Change CoreUpdaterInfo.bat

--- src/MakePdf.Wpf/CoreUpdaterInfo.bat
+++ src/MakePdf.Wpf/CoreUpdaterInfo.bat
@@ -2,5 +2,5 @@ cd %~dp0
 set TOOL=%~dp0..\..\..\CoreUpdater\src\CoreUpdater.Console\bin\Release\netcoreapp2.0\CoreUpdater.Console.dll
 set TARGET=%~dp0bin\MakePdf
 set NAME=MakePdf
-set VERSION=0.17.0
+set VERSION=0.17.1
 dotnet %TOOL% -d=%TARGET% -n=%NAME% -v=%VERSION%
